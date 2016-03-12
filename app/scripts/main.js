06c5918b7b09e261ea94b3c47a4a1bf5029de57a
Alex Ong <aong@thoughtworks.com>
2016-03-12 22:50:36 +1100

    created field for github api token; loading CSV

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+function loadData() {
+  $.get("data/projects.csv", function(response) {
+    var projects = Papa.parse(response, { header: true });
+    console.log(projects);
+  });
+}
+
+$(".generate").click(loadData);
